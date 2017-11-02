05fd979daac83b9fb592aa90e61ba30f0eb13e9c
Phil Turner <julietius@gmail.com>
2017-11-02 21:07:08 +0000

    add frontend route and basic form block

---
+++ Controller/Index/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace PhilTurner\GroupPricesInvite\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->resultPageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+
+        return $resultPage;
+    }
+}
