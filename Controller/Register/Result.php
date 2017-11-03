b1b8b2308727398240d8b8626686a28a79afd9b1
Phil Turner <julietius@gmail.com>
2017-11-03 22:07:02 +0000

    add collection factory to validate user code with db

--- Controller/Register/Result.php
+++ Controller/Register/Result.php
@@ -1,40 +1,68 @@
 <?php
 namespace PhilTurner\GroupPricesInvite\Controller\Register;
 
+use Magento\Framework\App\Action\Context;
+use PhilTurner\GroupPricesInvite\Model\ResourceModel\Company\CollectionFactory;
+
 class Result extends \Magento\Framework\App\Action\Action
 {
-    protected $validCodes = [
-        '1111',
-        '2222',
-        '3333',
-    ];
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
 
     protected $userCode;
 
+    protected $validCodes;
+
+    /**
+     * @param Context $context
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, CollectionFactory $collectionFactory)
+    {
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     *
+     */
     private function isValidCompanyCode()
     {
-        foreach ($this->validCodes as $codes) {
-            if ($codes == $this->userCode){
+        foreach ($this->getValidCodes() as $codes) {
+            if (strtolower($codes) == strtolower($this->userCode)){
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     *
+     */
+    private function getValidCodes()
+    {
+        $collection = $this->collectionFactory->create();
+        $this->validCodes = $collection->getColumnValues('invitation_code');
+        return $this->validCodes;
+    }
+
+    /**
+     *
+     */
     public function execute()
     {
-
         $this->userCode = $this->getRequest()->getParam('unique_code');
 
-        if ($this->isValidCompanyCode($this->userCode)){
-            echo ("Code is a valid one");
+        if ($this->isValidCompanyCode()){
+            echo ("\nCode is a valid one - Customer Invitation to be sent.");
         } else {
-            echo ("You did not enter a valid code");
+            echo ("\n\nCode NOT valid - No invitation to be sent.");
         }
 
         $this->_view->loadLayout();
         $this->_view->getLayout()->initMessages();
         $this->_view->renderLayout();
-
     }
 }
